1aa809e312d47ce145c2e4c699abbf9e209b53f6
Shahid Dhariwala <shahid.dhariwala@gmail.com>
2020-10-15 08:30:24 +0530

    File Strcuture and Header

---
+++ task-manager/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Styles from "./index.module.css";
+
+const LinearProgressWithLabel = (props) => {
+    return (
+        <div className={Styles.outerContainer}>
+            Task Manager
+        </div>
+    );
+}
+
+export default LinearProgressWithLabel;
